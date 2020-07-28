b73afe7255b40cb1ff54675d912631879b84ae3e
Darien Hong <darienlake2009@hotmail.com>
2020-07-27 20:49:37 -0400

    changed title

--- src/footer.jsx
+++ src/footer.jsx
@@ -16,7 +16,7 @@ export default class Footer extends Component {
 
             <div class="footer-block">
             <footer > 
-                Developed and designed with &hearts; by Darien Hong &copy; 2020 
+                Developed and designed with &#9829; by Darien Hong &copy; 2020 
             </footer>
             </div> 
 
